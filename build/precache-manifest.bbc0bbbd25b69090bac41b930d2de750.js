e5e81d6c66ce9d2b0b1c821963fa002ce3e465d9
Kedar <60250669+kedar6160@users.noreply.github.com>
2022-12-29 14:06:49 +0530

    Add files via upload
    Project new build files

---
+++ build/precache-manifest.bbc0bbbd25b69090bac41b930d2de750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fc7f07c08ab1895d8b9d6770935043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a78b244343d7de3ae4d5",
+    "url": "/static/css/main.66bd2285.chunk.css"
+  },
+  {
+    "revision": "34b12189656f86f6930b",
+    "url": "/static/js/2.b04848ba.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.b04848ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78b244343d7de3ae4d5",
+    "url": "/static/js/main.13a3f97c.chunk.js"
+  },
+  {
+    "revision": "8ecd99f0b9902236233a",
+    "url": "/static/js/runtime-main.1f1dc415.js"
+  }
+]);
